7b10e859f27d38cb73d1d1a08ca11277526b07ee
tugang <15994738101@163.com>
2018-10-31 10:41:10 +0800

    时间比较

--- src/com/tugang/demo/Demo.java
+++ src/com/tugang/demo/Demo.java
@@ -1,7 +1,24 @@
 package com.tugang.demo;
 
+import java.util.Date;
+
 public class Demo {
 	public static void main(String[] args) {
-		System.out.println("========");
+		try {
+			Date d1 = new Date();
+			Thread.sleep(1000);
+			Date d2 = new Date();
+			
+			if (d1.compareTo(d2) == 0) {
+				System.out.println("=");
+			} else if (d1.compareTo(d2) > 0) {
+				System.out.println(">");
+			} else {
+				System.out.println("<");
+			}
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 }
